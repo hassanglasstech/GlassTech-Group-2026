ad00b6ca7268e387ad62515474c6e1637ef0dc0c
hassanglasstech <hassanlatif1302@gmail.com>
2026-03-29 23:39:56 +0500

    Update Requisitions.tsx

--- modules/procurement/pages/Requisitions.tsx
+++ modules/procurement/pages/Requisitions.tsx
@@ -1042,7 +1042,6 @@ const Requisitions: React.FC = () => {
                           className={`w-12 h-6 rounded-full transition-colors relative ${formHeader.requiresCashPayment ? 'bg-emerald-500' : 'bg-slate-200'}`}>
                           <span className={`absolute top-0.5 w-5 h-5 bg-white rounded-full shadow transition-transform ${formHeader.requiresCashPayment ? 'translate-x-6' : 'translate-x-0.5'}`} />
                         </button>
-                        </button>
                       </div>
                     </div>
                     <div className="bg-white p-5 rounded-3xl border shadow-sm">
